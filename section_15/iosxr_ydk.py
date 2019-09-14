5a655c8bfe3a4382c5bec98610bff66725dd4623
denis <git@dpnet.ca>
2019-09-14 10:07:58 -0600

    added section 15

---
+++ section_15/iosxr_ydk.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from ydk.services import CRUDService
+from ydk.providers import NetconfServiceProvider
+from ydk.models.openconfig import bgp
+
+sbx_iosxr_ao = {
+    "host": "sbx-iosxr-mgmt.cisco.com",
+    "port": 10000,
+    "username": "admin",
+    "protocol": "ssh",
+}
+
+if __name__ == "__main__":
+    provider = NetconfServiceProvider(**sbx_iosxr_ao)
+    crud = CRUDService()
+    bgp = bgp.Bgp()
+    bgp.global_.config.as_ = 65000
+    crud.create(provider, bgp)
+    provider.close()
